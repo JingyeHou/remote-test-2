89cf9926c10525cbc954a0c5e55853210f44f84a
JingyeHou <jingyehou@gmail.com>
2017-10-30 22:14:24 -0400

    conflict

---
+++ Demo.java
@@ -0,0 +1,9 @@
+Demo class {
+    public static void main(String[] args) {
+        Demo demo = new Demo();
+        demo.run();
+    }
+    public run() {
+        System.out.println("jingyehou");
+    }
+}
